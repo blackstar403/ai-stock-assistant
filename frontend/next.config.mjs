87ccc098b1fd0ab678476693876c2ce4fc0e8e41
Ben <ben_zzz@163.com>
2025-03-14 20:29:37 +0800

    fix: Updated the Dockerfile to use Next.js in standalone mode

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: 'standalone',
   async rewrites() {
     return [
-      // {
-      //   source: '/api/:path*',
-      //   destination: 'http://localhost:8000/api/:path*', // 指向后端API
-      // },
+      {
+        source: '/api/:path*',
+        destination: 'http://backend:8000/api/:path*', // 指向Docker网络中的后端服务
+      },
     ];
   },
 };
